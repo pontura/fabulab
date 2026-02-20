89f3b67e6de332e70ee9301869d2c8ca5ebf034b
Juan Pablo Amato <jpamato@users.noreply.github.com>
2026-02-20 16:00:02 -0300

    fix deserialization errors

--- Assets/scripts/boardItems/AlbumData.cs
+++ Assets/scripts/boardItems/AlbumData.cs
@@ -128,11 +128,12 @@ namespace BoardItems
             }
 
             public void LoadServerData(ServerIData serverData) {
+                id = serverData.id;
                 galleryID = serverData.galleryID;
                 part = serverData.part;
-                position = new Vector3(serverData.position.x, serverData.position.x, serverData.position.z);
-                rotation = new Vector3(serverData.rotation.x, serverData.rotation.x, serverData.rotation.z);
-                scale = new Vector3(serverData.scale.x, serverData.scale.x, serverData.scale.z);
+                position = new Vector3(serverData.position.x, serverData.position.y, serverData.position.z);
+                rotation = new Vector3(serverData.rotation.x, serverData.rotation.y, serverData.rotation.z);
+                scale = new Vector3(serverData.scale.x, serverData.scale.y, serverData.scale.z);
                 anim = serverData.anim;
                 color = serverData.color;
                 Debug.Log(galleryID + ", " + part + ", " + position + ", " + rotation + ", " + scale + ", " + anim + ", " + color);
