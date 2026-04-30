cbbbb8e8039482bb5cb42c441340a485ab41169b
Agustin Pontura <pontura@gmail.com>
2026-04-30 16:45:17 -0300

    player con textos

--- Assets/scripts/StoryMaker/VideoPlayerFabulab.cs
+++ Assets/scripts/StoryMaker/VideoPlayerFabulab.cs
@@ -1,9 +1,16 @@
-using Google.MiniJSON;
+using BoardItems;
 using UnityEngine;
+using UnityEngine.UI;
+
 namespace Yaguar.StoryMaker.Editor
 {
     public class VideoPlayerFabulab : MonoBehaviour
     {
+        [SerializeField] TMPro.TMP_Text titleField;
+        [SerializeField] TMPro.TMP_Text creatorField;
+        [SerializeField] TMPro.TMP_Text dateField;
+        [SerializeField] Image image;
+
         [SerializeField] FilmMakerManagerFabulab filmMakerManagerFabulab;
         [SerializeField] TimelineFabulab timeline;
         [SerializeField] GameObject playButton;
@@ -11,6 +18,12 @@ namespace Yaguar.StoryMaker.Editor
         public void Show(bool isOn)
         {
             gameObject.SetActive(isOn);
+            if (isOn)
+            {
+                playButton.SetActive(true);
+                FilmDataFabulab f = Data.Instance.scenesData.currentFilmData;
+                SetContent(f);
+            }
         }
         public void PlayClicked()
         {
@@ -28,6 +41,19 @@ namespace Yaguar.StoryMaker.Editor
         {
             filmMakerManagerFabulab.Prev();
             StoryMakerEvents.OnTimelinePlay(true);
+        }
+        void SetContent(FilmDataFabulab content)
+        {
+            titleField.text = content.name;
+
+            Data.Instance.cacheData.GetUser(content.userID, (userData) => {
+                creatorField.text = userData.username;
+                image.sprite = userData.thumb != null ? Sprite.Create(userData.thumb, new Rect(0, 0, userData.thumb.width, userData.thumb.height), new Vector2(0.5f, 0.5f)) : null;
+
+            });
+
+            dateField.text = content.GetTimestamp();
+
         }
     }
 }
