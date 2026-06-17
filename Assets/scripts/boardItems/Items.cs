a63e42f75da60e2cbac92c6f1252440840081172
Juan Pablo Amato <jpamato@users.noreply.github.com>
2026-06-17 15:43:06 -0300

    Can load bg with so in stories

--- Assets/scripts/boardItems/Items.cs
+++ Assets/scripts/boardItems/Items.cs
@@ -668,60 +668,62 @@ namespace BoardItems
             foreach (SavedIData soID in soIDs) {
              //   print("open SO in background soID: " + soID.soID);
                 SOPartData soPartData = Data.Instance.sObjectsData.GetSO(soID.soID);
-                if(soPartData == null)
-                    soPartData = Data.Instance.sObjectsData.GetOthersSO(soID.soID);
-
-                if (soPartData != null) {
-                    GameObject go = new GameObject();
-                    BoardItemManager boardItemManager_to_add = go.AddComponent<BoardItemManager>();
-
-                    if (container == null)
-                        container = boardItemManager.GetBodyPart((CharacterPartsHelper.parts)soID.part).transform;
-                    Destroy(go.gameObject);
-
-                    //AddSceneObjectTo(o, boardItemManager_to_add, container, soID);
-                    //AddSceneObjectTo(SOPartData wd, BoardItemManager newBoardItemManager, Transform container, SavedIData savedIOData = null)
-
-                    BoardItemManager childBoardItemManager = Instantiate(boardItemManager_to_add, container);
-                    childBoardItemManager.name = "Object " + soPartData.id;
-                    childBoardItemManager.transform.SetParent(container);
-                    childBoardItemManager.transform.localPosition = Vector3.zero;
-                    ItemData newItem;
-                    List<GameObject> childs = new List<GameObject>();
-                    float _z = 0;
-                    foreach (SavedIData itemData in soPartData.items) {
-                        _z -= 0.001f;
-                        itemData.position.z = _z;
-                        newItem = CreateItem(itemData, boardItemManager_to_add);
-                        newItem.transform.SetParent(childBoardItemManager.transform);
-                        childs.Add(newItem.gameObject);
-                    }
-
-                    float scale = bgContainer.localScale.x;
-                    float offsetY = bgContainer.localPosition.y;
-                    childBoardItemManager.transform.localScale = new Vector3(8 * scale, 8 * scale, 1);
-                    childBoardItemManager.transform.localPosition = new Vector3(0, offsetY, 0);
-
-                    foreach (GameObject gO in childs)
-                        gO.transform.SetParent(transform);
-
-                    childBoardItemManager.transform.localScale = new Vector3(1f, 1f, 1f);
-                    childBoardItemManager.transform.localPosition = new Vector3(childBoardItemManager.transform.localPosition.x, 0, 0);
-                    foreach (GameObject gO in childs)
-                        gO.transform.SetParent(childBoardItemManager.transform);
-
-
-                    //float snapScale = Data.Instance.settings.snapScale*8;
-                    //float scale = snapScale * (8 / snapScale);
-                    //UIManager.Instance.boardUI.items.ScaleSnaped(-snapScale);
-
-                    childBoardItemManager.SetInteractableObject(soPartData.id, OnObjectMerged, soID);
+                if(soPartData != null) {
+                    LoadBgSo(soID, soPartData, boardItemManager, bgContainer, container);
                 } else {
-                    print("open work itemData.soID not found: " + soID);
-                }
-            }
-            
+                    Data.Instance.sObjectsData.LoadOthersObject(soID.soID, (partData) => {
+                        LoadBgSo(soID, partData, boardItemManager, bgContainer, container);
+                    });
+                }                    
+            }            
             bgContainer.localScale *= 0.5f;
+        }
+
+        void LoadBgSo(SavedIData soID, SOPartData soPartData, BoardItemManager boardItemManager, Transform bgContainer, Transform container) {
+            GameObject go = new GameObject();
+            BoardItemManager boardItemManager_to_add = go.AddComponent<BoardItemManager>();
+
+            if (container == null)
+                container = boardItemManager.GetBodyPart((CharacterPartsHelper.parts)soID.part).transform;
+            Destroy(go.gameObject);
+
+            //AddSceneObjectTo(o, boardItemManager_to_add, container, soID);
+            //AddSceneObjectTo(SOPartData wd, BoardItemManager newBoardItemManager, Transform container, SavedIData savedIOData = null)
+
+            BoardItemManager childBoardItemManager = Instantiate(boardItemManager_to_add, container);
+            childBoardItemManager.name = "Object " + soPartData.id;
+            childBoardItemManager.transform.SetParent(container);
+            childBoardItemManager.transform.localPosition = Vector3.zero;
+            ItemData newItem;
+            List<GameObject> childs = new List<GameObject>();
+            float _z = 0;
+            foreach (SavedIData itemData in soPartData.items) {
+                _z -= 0.001f;
+                itemData.position.z = _z;
+                newItem = CreateItem(itemData, boardItemManager_to_add);
+                newItem.transform.SetParent(childBoardItemManager.transform);
+                childs.Add(newItem.gameObject);
+            }
+
+            float scale = bgContainer.localScale.x;
+            float offsetY = bgContainer.localPosition.y;
+            childBoardItemManager.transform.localScale = new Vector3(8 * scale, 8 * scale, 1);
+            childBoardItemManager.transform.localPosition = new Vector3(0, offsetY, 0);
+
+            foreach (GameObject gO in childs)
+                gO.transform.SetParent(transform);
+
+            childBoardItemManager.transform.localScale = new Vector3(1f, 1f, 1f);
+            childBoardItemManager.transform.localPosition = new Vector3(childBoardItemManager.transform.localPosition.x, 0, 0);
+            foreach (GameObject gO in childs)
+                gO.transform.SetParent(childBoardItemManager.transform);
+
+
+            //float snapScale = Data.Instance.settings.snapScale*8;
+            //float scale = snapScale * (8 / snapScale);
+            //UIManager.Instance.boardUI.items.ScaleSnaped(-snapScale);
+
+            childBoardItemManager.SetInteractableObject(soPartData.id, OnObjectMerged, soID);
         }
 
         IEnumerator Cascade(BoardItemManager boardItemManager, SOPartData wd)
