d637c7d4b3a8e272bf55fbb5ef553120cb537094
Agustin Pontura <pontura@gmail.com>
2026-09-24 14:57:38 -0300

    stories fixed

--- Assets/scripts/ui/mainApp/StoryEditorScreen.cs
+++ Assets/scripts/ui/mainApp/StoryEditorScreen.cs
@@ -66,6 +66,10 @@ namespace UI.MainApp
             characterScreen.Show(false);
             backgroundScreen.Show(false);
             objectsScreen.Show(false);
+            toolsStories?.Close();
+            arrowSelect?.SetActive(false);
+            editAvatar?.SetActive(false);
+            editObjects?.SetActive(false);
          }
         void OnDestroy()
         {
@@ -89,6 +93,7 @@ namespace UI.MainApp
         private void OnEnable()
         {
             Invoke(nameof(Init), Time.deltaTime * 4);
+            toolsStories.Close();
         }
 
         public void Init()
