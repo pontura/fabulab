97055fb35bfe578b881eeb0a02695a695870e8e9
Juan Pablo Amato <jpamato@users.noreply.github.com>
2026-09-24 16:21:54 -0300

    merge and metas

---
+++ Assets/scripts/boardItems/ToolsMenuStories.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace BoardItems
+{
+    public class ToolsMenuStories : MonoBehaviour
+    {
+        public GameObject arrow;
+        public void Init(Vector3 pos) {
+            gameObject.SetActive(true);
+            arrow.transform.position = pos;
+        }
+        public void Close()
+        {
+            gameObject.SetActive(false);            
+        }
+    }
+}
